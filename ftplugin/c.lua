6eecec5b00930faf676491f8502e37c1e406286a
Hardy Jimenez Matos <hardy.rafael17@gmail.com>
2022-11-24 02:12:25 +0100

    first commit

---
+++ ftplugin/c.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
+
+-- Tabs, indent
+-----------------------------------------------------------
+opt.expandtab = false        -- Use spaces instead of tabs
+opt.smartindent = true      -- Autoindent new lines
+opt.shiftwidth = 4
+
---
+++ ftplugin/make.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
+
+-- Tabs, indent
+-----------------------------------------------------------
+opt.expandtab = false        -- Use spaces instead of tabs
+opt.tabstop = 4             -- 1 tab == 4 spaces
+opt.smartindent = true      -- Autoindent new lines
+
